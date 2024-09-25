6a385726c202bb8524bb24fa4d9d8e3b70df1b0b
Martin Granc3k Simon <mart.simon17@gmail.com>
2024-09-25 14:46:09 +0200

    formating

--- cv01/cv01.py
+++ cv01/cv01.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import struct
 
+
 def main():
-    with open('./cv01/cv01_dobryden.wav', 'rb') as f:
+    with open("./cv01/cv01_dobryden.wav", "rb") as f:
         data = f.read(4)
         print("data: " + str(data))
-        RIFF = struct.unpack('i', f.read(4))[0]
+        RIFF = struct.unpack("i", f.read(4))[0]
         print("RIFF: " + str(RIFF))
         WAVE = f.read(4)
         print("WAVE: " + str(WAVE))
@@ -19,9 +20,9 @@ def main():
         print("format: " + str(format))
         channels = f.read(2)
         print("kanálů: " + str(channels))
-        VF = struct.unpack('i', f.read(4))[0]
+        VF = struct.unpack("i", f.read(4))[0]
         print("VF: " + str(VF))
-        PB = struct.unpack('i', f.read(4))[0]
+        PB = struct.unpack("i", f.read(4))[0]
         print("PB: " + str(PB))
         VB = f.read(2)
         print("VB: " + str(VB))
@@ -29,16 +30,17 @@ def main():
         print("VV: " + str(VV))
         data = f.read(4)
         print("data: " + str(data))
-        A2 = struct.unpack('i', f.read(4))[0]
+        A2 = struct.unpack("i", f.read(4))[0]
         print("A2: " + str(A2))
         SIG = np.zeros(A2)
         for i in range(0, A2):
-            SIG[i] = struct.unpack('B', f.read(1))[0]
-    t = np.arange(A2).astype(float)/VF
+            SIG[i] = struct.unpack("B", f.read(1))[0]
+    t = np.arange(A2).astype(float) / VF
     plt.plot(t, SIG)
-    plt.xlabel('t[s]')
-    plt.ylabel('A[-]')
+    plt.xlabel("t[s]")
+    plt.ylabel("A[-]")
     plt.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
